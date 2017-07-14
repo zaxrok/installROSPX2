a84e2c13282d62855fd70c7e98c9c755c6ec7512
DavidGogh <davidgogh96@gmail.com>
2017-07-14 15:06:56 +0800

    Modify the scripts to use them on Nvidia PX2

--- installROS.sh
+++ installROS.sh
@@ -4,13 +4,18 @@
 # Information from:
 # http://wiki.ros.org/kinetic/Installation/UbuntuARM
 
+# The script is modified according to discussions on the Nvidia forum: https://devtalk.nvidia.com/default/topic/1011002/general/cross-compiling-driveworks-on-the-px2-with-ros/
+
 # Setup sources.lst
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+# Update the libssl1.0.0 package to the latest version [1.0.2g-1ubuntu4.6]and install libssl-dev
+sudo apt-get install libssl1.0.0/xenial libssl-doc/xenial libssl-dev/xenial
 # Installation
-sudo apt-get update
-sudo apt-get install ros-kinetic-ros-base -y
+# sudo apt-get update
+# sudo apt-get install ros-kinetic-ros-base -y
+sudo apt-get install ros-kinetic-desktop
 # Add Individual Packages here
 # You can install a specific ROS package (replace underscores with dashes of the package name):
 # sudo apt-get install ros-kinetic-PACKAGE
@@ -30,6 +35,7 @@ sudo rosdep init
 rosdep update
 # Environment Setup
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+# echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib/aarch64-linux-gnu" >> ~/.bashrc
 source ~/.bashrc
 # Install rosinstall
 sudo apt-get install python-rosinstall -y
--- updateRepositories.sh
+++ updateRepositories.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Update the needed repositories for the ROS on the Jetson
+# Update the needed repositories for the ROS on the PX2
 # Configure repositories
 sudo apt-add-repository universe
 sudo apt-add-repository multiverse
 sudo apt-add-repository restricted
-sudo apt-get update
+#sudo apt-get update
 
